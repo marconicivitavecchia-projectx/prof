8202e167713745ae046b2c5c1bc2ef83e6dc909f
Claudio Capobianco <claudio.capobianco@istruzione.it>
2019-02-12 11:44:40 +0100

    Add web app template

---
+++ code/app.js
@@ -0,0 +1,23 @@
+app = {
+    init : function() {
+        console.log("init inside app!");
+        $("title").text("Esempio by prof");
+        $.get("data.json")
+        .done(app.onSuccess)
+        .fail(app.onError);
+        
+    },
+    onSuccess:function(jsonData){
+        // processa i dati qui!!!
+        console.log(jsonData);
+    },
+    
+    onError:function(e){
+        console.log("error!");
+        console.log(JSON.stringify(e));
+    }
+};
+
+
+
+$(document).ready(app.init);
