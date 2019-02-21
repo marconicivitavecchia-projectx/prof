e3dedf5cfb44e8046fd59ab8a99940005c51039d
wbigger <wbigger@gmail.com>
2019-02-21 11:53:43 +0100

    Make page dynamic with json and js

--- code/app.js
+++ code/app.js
@@ -2,7 +2,7 @@ app = {
     init : function() {
         console.log("init inside app!");
         $("title").text("Project X");
-        $.get("data.json")
+        $.getJSON("data.json")
         .done(app.onSuccess)
         .fail(app.onError);
         
@@ -10,6 +10,14 @@ app = {
     onSuccess:function(jsonData){
         // processa i dati qui!!!
         console.log(jsonData);
+        let list = jsonData.commandList;
+        list.forEach(element => {
+            let html = `<div class="table-row">
+            <div class="table-cell">${element.name}</div>
+            <div class="table-cell">${element.description}</div>
+        </div>`;
+        $("#command-table").append(html);
+        });
     },
     
     onError:function(e){
